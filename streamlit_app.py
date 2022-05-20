04e44727e5feafbc3af59cf21ded0c453e06afa5
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-20 11:39:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,7 @@ with st.echo(code_location='below'):
     
     chart_data = pd.DataFrame(
         [x['SWAP_FEES'] for x in data],
-        [x['DAY'] for x in data]
+        [x['DAY'] for x in data],
+        [x['POOL_NAME'] for x in data]
     )
     st.bar_chart(chart_data)
