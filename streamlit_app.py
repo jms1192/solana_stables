94c02980a10979fe4ce56afebbbe059c8a1e5522
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-20 12:13:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,6 @@ with st.echo(code_location='below'):
     
     symbols = st.multiselect("Choose stocks to visualize", all_symbols, all_symbols[:3])
 
-    space(1)
-
     source = source[source.symbol.isin(symbols)]
     chart = chart.get_chart(source)
     st.altair_chart(chart, use_container_width=True)
