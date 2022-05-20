d8f5bfb464c0116557e9e229807b401782c7d67e
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-20 12:44:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,8 @@ with st.echo(code_location='below'):
 
     source = pd.DataFrame(
         [x['SWAP_VOLUME'] for x in data if x['ASSET'] in symbols],
-        [x['DAY'] for x in data if x['ASSET'] in symbols]
+        [x['DAY'] for x in data if x['ASSET'] in symbols],
+        symbols
     )
     ##chart = chart.get_chart(chart_data)
     
