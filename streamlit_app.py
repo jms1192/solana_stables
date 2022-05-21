21c0985d938cd1d412494fa83264a27985df9478
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-21 15:28:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,67 +32,63 @@ Yieldly is a DeFi staking protocol built for Algorand, that allows holders of Al
 
 """
 
-with st.echo(code_location='below'):
+##with st.echo(code_location='below'):
     
   
-    ### pick symbols
-    data = requests.get('https://node-api.flipsidecrypto.com/api/v2/queries/5c5ecaee-e9da-4ff6-b62d-651711f9d324/data/latest').json()
-    symbols = [x['ASSET'] for x in data]
-    all_symbols = []
-    for i in symbols:
-        if i not in all_symbols:
-            all_symbols.append(i)
+### pick symbols
+data = requests.get('https://node-api.flipsidecrypto.com/api/v2/queries/5c5ecaee-e9da-4ff6-b62d-651711f9d324/data/latest').json()
+symbols = [x['ASSET'] for x in data]
+all_symbols = []
+for i in symbols:
+    if i not in all_symbols:
+        all_symbols.append(i)
     
-    symbols = st.multiselect("Choose asset to visualize", all_symbols, all_symbols[:9])
-    st.text("")
+symbols = st.multiselect("Choose asset to visualize", all_symbols, all_symbols[:9])
+st.text("")
     
-    ### Sort Data  
-    vol_data = {}
-    vol_data_sum = {}
-    date_data = []
-    vol = []
-    asset = ''
-    for x in data:
-        if x['ASSET'] in symbols:
-            if not x['ASSET'] == asset:
-                if asset == '':
-                    asset = x['ASSET']
-        
-                else:
-                    vol_data_sum[asset] = sum(vol)
-                    vol_data[asset] = vol
-                    asset = x['ASSET']
-                  
-                vol = []
-                vol.append(x['SWAP_VOLUME'])
-                date_data = []
-                date_data.append(x['DAY'])
+### Sort Data  
+vol_data = {}
+vol_data_sum = {}
+date_data = []
+vol = []
+asset = ''
+for x in data:
+    if x['ASSET'] in symbols:
+        if not x['ASSET'] == asset:
+            if asset == '':
+                asset = x['ASSET']
             else:
-                date_data.append(x['DAY'])
-                vol.append(x['SWAP_VOLUME'])
-    vol_data[asset] = vol           
+                vol_data_sum[asset] = sum(vol)
+                vol_data[asset] = vol
+                asset = x['ASSET']
+                  
+            vol = []
+            vol.append(x['SWAP_VOLUME'])
+            date_data = []
+            date_data.append(x['DAY'])
+        else:
+            date_data.append(x['DAY'])
+            vol.append(x['SWAP_VOLUME'])
+            
+vol_data[asset] = vol           
                 
-    symbols.sort()
-    ### Display Bar chart
+symbols.sort()
+### Display Bar chart
     
-    chart = pd.DataFrame(
-        vol_data,
-        index=date_data
-    )
-    
-    
-    ### Pie chart 
-  
+chart = pd.DataFrame(
+    vol_data,
+    index=date_data
+)
+   
+### Pie chart 
         
-    chart2 = pd.DataFrame(
-       [sum(vol_data[x]) for x in vol_data],
-       ##vol_data_sum
-       index=[x for x in vol_data]
-    )
-    
-    
-    st.bar_chart(chart)
-    st.bar_chart(chart2)
+chart2 = pd.DataFrame(
+    [sum(vol_data[x]) for x in vol_data],
+    index=[x for x in vol_data]
+)
+   
+st.bar_chart(chart)
+st.bar_chart(chart2)
 """
 ### Observations 
 
