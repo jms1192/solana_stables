894f9d3d965c20207f4e6fb32d78dcb7b55e6894
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-21 13:30:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import requests
+import matplotlib.pyplot as plt
 
 """
 # Swapping for YLDLY on DEXs
@@ -73,10 +74,23 @@ with st.echo(code_location='below'):
     vol_data[asset] = vol           
                 
     symbols.sort()
-    ### Display chart
+    ### Display Bar chart
     
     chart = pd.DataFrame(
         vol_data,
         index=date_data
     )
     st.bar_chart(chart)
+    
+    ### Pie chart 
+    
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    st.pyplot(fig1)
