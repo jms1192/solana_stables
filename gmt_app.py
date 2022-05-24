bffb0fd950903e729e2a6190c92e834720878d7b
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-05-24 11:21:36 -0400

    Update gmt_app.py

--- gmt_app.py
+++ gmt_app.py
@@ -1,146 +1,223 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 import numpy as np
-import requests
+from pandas import DataFrame
+from keybert import KeyBERT
+# For Flair (Keybert)
+from flair.embeddings import TransformerDocumentEmbeddings
+import seaborn as sns
+# For download buttons
+from functionforDownloadButtons import download_button
+import os
+import json
 
-"""
-# Tracking GMT into StepN
+st.set_page_config(
+    page_title="BERT Keyword Extractor",
+    page_icon="🎈",
+)
 
-Is swapping for YLDY more popular on some DEXs than others? 
-What assets are users swapping for YLDLY?
-Are there any standout days of swap volume for Yieldly?
-### What is YLDLY?
-Yieldly is a DeFi staking protocol built for Algorand, that allows holders of Algo to stake their assets and have the chance to earn a disproportionate reward. The native token of the Yieldly ecosystem is YLDLY.
-### Methodology
-- We will look at the YLDLY token swap volume in terms of swap to asset and DEX. 
-- We are looking to see where and what assets people are swapping with YLDlY in the passt 30 days.
-## YLDLY token swap metrics 
-"""
 
-##with st.echo(code_location='below'):
-    
-  
-### pick symbols
-data = requests.get('https://node-api.flipsidecrypto.com/api/v2/queries/5c5ecaee-e9da-4ff6-b62d-651711f9d324/data/latest').json()
-symbols = [x['ASSET'] for x in data]
-all_symbols = []
-for i in symbols:
-    if i not in all_symbols:
-        all_symbols.append(i)
-    
-symbols = st.multiselect("Choose asset to visualize", all_symbols, all_symbols[:9])
-st.text("")
-    
-### Sort Data  
-vol_data = {}
-vol_data_sum = {}
-date_data = []
-vol = []
-asset = ''
-for x in data:
-    if x['ASSET'] in symbols:
-        if not x['ASSET'] == asset:
-            if asset == '':
-                asset = x['ASSET']
-            else:
-                vol_data_sum[asset] = sum(vol)
-                vol_data[asset] = vol
-                asset = x['ASSET']
-                  
-            vol = []
-            vol.append(x['SWAP_VOLUME'])
-            date_data = []
-            date_data.append(x['DAY'])
+def _max_width_():
+    max_width_str = f"max-width: 1400px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+_max_width_()
+
+c30, c31, c32 = st.columns([2.5, 1, 3])
+
+with c30:
+    # st.image("logo.png", width=400)
+    st.title("🔑 BERT Keyword Extractor")
+    st.header("")
+
+
+
+with st.expander("ℹ️ - About this app", expanded=True):
+
+    st.write(
+        """     
+-   The *BERT Keyword Extractor* app is an easy-to-use interface built in Streamlit for the amazing [KeyBERT](https://github.com/MaartenGr/KeyBERT) library from Maarten Grootendorst!
+-   It uses a minimal keyword extraction technique that leverages multiple NLP embeddings and relies on [Transformers] (https://huggingface.co/transformers/) 🤗 to create keywords/keyphrases that are most similar to a document.
+	    """
+    )
+
+    st.markdown("")
+
+st.markdown("")
+st.markdown("## **📌 Paste document **")
+with st.form(key="my_form"):
+
+
+    ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
+    with c1:
+        ModelType = st.radio(
+            "Choose your model",
+            ["DistilBERT (Default)", "Flair"],
+            help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
+        )
+
+        if ModelType == "Default (DistilBERT)":
+            # kw_model = KeyBERT(model=roberta)
+
+            @st.cache(allow_output_mutation=True)
+            def load_model():
+                return KeyBERT(model=roberta)
+
+            kw_model = load_model()
+
         else:
-            date_data.append(x['DAY'])
-            vol.append(x['SWAP_VOLUME'])
-            
-vol_data[asset] = vol           
-                
-symbols.sort()
-### Display Bar chart
-    
-chart = pd.DataFrame(
-    vol_data,
-    index=date_data
-)
-   
-### Pie chart 
-        
-chart2 = pd.DataFrame(
-    [sum(vol_data[x]) for x in vol_data],
-    index=[x for x in vol_data]
-)
-   
-st.bar_chart(chart)
-st.bar_chart(chart2)
-"""
-### Observations 
-- The vast majority of all YLDLY swap volume was with the ALGO token.
-- The YLDLY token's swap volume has been declining over the past 30 days.
-"""
+            @st.cache(allow_output_mutation=True)
+            def load_model():
+                return KeyBERT("distilbert-base-nli-mean-tokens")
 
-data = requests.get('https://node-api.flipsidecrypto.com/api/v2/queries/1cbd998e-fe68-4992-bd31-5c204fdf426a/data/latest').json()
-symbols = [x['ASSET'] for x in data]
-all_symbols = []
-for i in symbols:
-    if i not in all_symbols:
-        all_symbols.append(i)
-    
-symbols = st.multiselect("Choose DEX to visualize", all_symbols, all_symbols[:9])
-st.text("")
-    
-### Sort Data  
-vol_data = {}
-vol_data_sum = {}
-date_data = []
-vol = []
-asset = ''
-for x in data:
-    if x['ASSET'] in symbols:
-        if not x['ASSET'] == asset:
-            if asset == '':
-                asset = x['ASSET']
-            else:
-                vol_data_sum[asset] = sum(vol)
-                vol_data[asset] = vol
-                asset = x['ASSET']
-                  
-            vol = []
-            vol.append(x['SWAP_VOLUME'])
-            date_data = []
-            date_data.append(x['DAY'])
-        else:
-            date_data.append(x['DAY'])
-            vol.append(x['SWAP_VOLUME'])
+            kw_model = load_model()
+
+        top_N = st.slider(
+            "# of results",
+            min_value=1,
+            max_value=30,
+            value=10,
+            help="You can choose the number of keywords/keyphrases to display. Between 1 and 30, default number is 10.",
+        )
+        min_Ngrams = st.number_input(
+            "Minimum Ngram",
+            min_value=1,
+            max_value=4,
+            help="""The minimum value for the ngram range.
+*Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
+To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases.""",
+            # help="Minimum value for the keyphrase_ngram_range. keyphrase_ngram_range sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set keyphrase_ngram_range to (1, # 2) or higher depending on the number of words you would like in the resulting keyphrases.",
+        )
+
+        max_Ngrams = st.number_input(
+            "Maximum Ngram",
+            value=2,
+            min_value=1,
+            max_value=4,
+            help="""The maximum value for the keyphrase_ngram_range.
+*Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
+To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases.""",
+        )
+
+        StopWordsCheckbox = st.checkbox(
+            "Remove stop words",
+            help="Tick this box to remove stop words from the document (currently English only)",
+        )
+
+        use_MMR = st.checkbox(
+            "Use MMR",
+            value=True,
+            help="You can use Maximal Margin Relevance (MMR) to diversify the results. It creates keywords/keyphrases based on cosine similarity. Try high/low 'Diversity' settings below for interesting variations.",
+        )
+
+        Diversity = st.slider(
+            "Keyword diversity (MMR only)",
+            value=0.5,
+            min_value=0.0,
+            max_value=1.0,
+            step=0.1,
+            help="""The higher the setting, the more diverse the keywords.
             
-vol_data[asset] = vol           
-                
-symbols.sort()
-### Display Bar chart
-    
-chart = pd.DataFrame(
-    vol_data,
-    index=date_data
+Note that the *Keyword diversity* slider only works if the *MMR* checkbox is ticked.
+""",
+        )
+
+    with c2:
+        doc = st.text_area(
+            "Paste your text below (max 500 words)",
+            height=510,
+        )
+
+        MAX_WORDS = 500
+        import re
+        res = len(re.findall(r"\w+", doc))
+        if res > MAX_WORDS:
+            st.warning(
+                "⚠️ Your text contains "
+                + str(res)
+                + " words."
+                + " Only the first 500 words will be reviewed. Stay tuned as increased allowance is coming! 😊"
+            )
+
+            doc = doc[:MAX_WORDS]
+
+        submit_button = st.form_submit_button(label="✨ Get me the data!")
+
+    if use_MMR:
+        mmr = True
+    else:
+        mmr = False
+
+    if StopWordsCheckbox:
+        StopWords = "english"
+    else:
+        StopWords = None
+
+if not submit_button:
+    st.stop()
+
+if min_Ngrams > max_Ngrams:
+    st.warning("min_Ngrams can't be greater than max_Ngrams")
+    st.stop()
+
+keywords = kw_model.extract_keywords(
+    doc,
+    keyphrase_ngram_range=(min_Ngrams, max_Ngrams),
+    use_mmr=mmr,
+    stop_words=StopWords,
+    top_n=top_N,
+    diversity=Diversity,
 )
-   
-### Pie chart 
-        
-chart2 = pd.DataFrame(
-    [sum(vol_data[x]) for x in vol_data],
-    index=[x for x in vol_data]
+
+st.markdown("## **🎈 Check & download results **")
+
+st.header("")
+
+cs, c1, c2, c3, cLast = st.columns([2, 1.5, 1.5, 1.5, 2])
+
+with c1:
+    CSVButton2 = download_button(keywords, "Data.csv", "📥 Download (.csv)")
+with c2:
+    CSVButton2 = download_button(keywords, "Data.txt", "📥 Download (.txt)")
+with c3:
+    CSVButton2 = download_button(keywords, "Data.json", "📥 Download (.json)")
+
+st.header("")
+
+df = (
+    DataFrame(keywords, columns=["Keyword/Keyphrase", "Relevancy"])
+    .sort_values(by="Relevancy", ascending=False)
+    .reset_index(drop=True)
 )
-   
-st.bar_chart(chart)
-st.bar_chart(chart2)
-    
-"""
-## Conclusion
-- Out of the 50 - 100 million YLDLY tokens swapped daily only between 1 and 3 million of them are swapped on DEXs other than Tinyman.
-- There were 2 days in the past month where the YLDLY token swap Volume was over 120 Million tokens.
-"""    
-  
-    
+
+df.index += 1
+
+# Add styling
+cmGreen = sns.light_palette("green", as_cmap=True)
+cmRed = sns.light_palette("red", as_cmap=True)
+df = df.style.background_gradient(
+    cmap=cmGreen,
+    subset=[
+        "Relevancy",
+    ],
+)
+
+c1, c2, c3 = st.columns([1, 3, 1])
+
+format_dictionary = {
+    "Relevancy": "{:.1%}",
+}
+
+df = df.format(format_dictionary)
+
+with c2:
+    st.table(df)
